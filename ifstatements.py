dd802cd2797031765675ecc60636d24bc03a041d
Maeve Foley <mfoley@cshnyc.org>
2018-09-20 11:56:17 -0400

    if statements

---
+++ ifstatements.py
@@ -0,0 +1,14 @@
+#if statements
+age = input("What is your age?")
+
+if age > 16:
+    print("you can drive")
+ #condition: compares things and tells us if it they are true or fals
+elif age < 16:
+    print("You'll be able to drive one day!")
+
+elif age == 16:
+    print("good luck on your drivers test")
+
+else:
+    print("not sure")
